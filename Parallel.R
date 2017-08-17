0c82913880b1150fb7bba23c42da2a5574d7d0df
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-08-17 10:20:19 +0300

    Added Parallel & began Scargle

---
+++ Parallel.R
@@ -0,0 +1,84 @@
+Parallel.Packages = function() return(c("foreach", "parallel", "doSNOW"))
+
+Parallel.Available = function()
+    return (all(Parallel.Packages() %in% installed.packages()))
+
+Parallel.TryInit = function(workers =
+    ifelse(("parallel" %in% sessionInfo()[[5]]) && exists("detectCores"), detectCores() - 2, 2))
+{
+    if (exists(".Cluster")) {
+        if ("cluster" %in% class(.Cluster)) {
+            message(sprintf("Detected cluster with %.0f nodes.", length(.Cluster)))
+            return(TRUE)
+        }
+        else {
+            message("Found variable '.Cluster' of different type. Cannot initialize.")
+            return(FALSE)
+        }
+    }
+
+    if (Parallel.Available() & (is.na(workers) | workers > 1)) {
+
+        sapply(Parallel.Packages(), library, character.only = TRUE)
+        .Cluster = makeCluster(workers, outfile = "./multithread.log")
+        assign(".Cluster", .Cluster, envir = .GlobalEnv)
+        registerDoSNOW(.Cluster)
+
+        message(sprintf("Registered %d nodes for parallel execution with doSNOW", length(.Cluster)))
+
+        return(TRUE)
+    }
+    else if (workers <= 1)
+        message(sprintf("Not enough workers. (Supplied value is '%g')", workers))
+    else {
+
+        s = "Parallel backend packages are not installed.\r\nMissing packages:"
+        index = 1
+        for (pckg in setdiff(Parallel.Packages(), installed.packages())) {
+            s = sprintf("%s\r\n%2.0f. %s", s, index, pckg)
+            index = index + 1
+        }
+
+        s = sprintf("%s\r\n", s)
+
+        message(s)
+    }
+
+    return(FALSE)
+
+}
+
+Parallel.BackendExists = function()
+    return (exists(".Cluster") && ("cluster" %in% class(.Cluster)))
+
+Parallel.TryDispose = function() {
+    if (!exists(".Cluster")) {
+        message("Cluster is not initialized.")
+        return(FALSE)
+    }
+    else if (!"cluster" %in% class(.Cluster)) {
+        message("Backend '.Cluster' variable is of different type. Aborting.")
+        return (FALSE)
+    }
+        
+
+    if (!("parallel" %in% sessionInfo()[[5]]) || !exists("stopCluster")) {
+        if ("parallel" %in% installed.packages()) {
+            message("Loading package 'parallel'")
+            library("parallel")
+        }
+        
+        else {
+            message("Package 'parallel' is unavailable. Aborting.")
+            return (FALSE)
+        }
+    }
+
+    stopCluster(.Cluster)
+
+    message("Cluster disposed.")
+
+    rm(".Cluster", envir = .GlobalEnv)
+
+    return (TRUE)
+}
---
+++ Scargle.R
@@ -0,0 +1,10 @@
+# Depends:
+#   Parallel.R
+
+
+Scargle.Tau = function(w, t, parallel = FALSE)
+{
+    if (Parallel.Available() && parallel) {
+
+    }
+}
