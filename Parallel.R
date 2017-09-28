8f399816c3d83c1c66fab958104af7499619a0cd
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-09-28 11:24:37 +0300

    Parallel RefClass implemented

--- Parallel.R
+++ Parallel.R
@@ -1,107 +1,75 @@
-Parallel.Packages = function() return(c("foreach", "parallel", "doSNOW"))
+Parallel.Packages = c("foreach", "parallel", "doSNOW")
 
 Parallel.Available = function()
-    return (all(Parallel.Packages() %in% installed.packages()))
+    return (all(Parallel.Packages %in% installed.packages()))
 
-Parallel.TryInit = function(workers =
-    ifelse(("parallel" %in% sessionInfo()[[5]]) && exists("detectCores"), detectCores() - 2, 2))
-{
-    if (exists(".Cluster")) {
-        if ("cluster" %in% class(.Cluster)) {
-            message(sprintf("Detected cluster with %.0f nodes.", length(.Cluster)))
-            return(TRUE)
-        }
-        else {
-            message("Found variable '.Cluster' of different type. Cannot initialize.")
-            return(FALSE)
-        }
-    }
 
-    if (Parallel.Available() & (is.na(workers) | workers > 1)) {
-
-        sapply(Parallel.Packages(), library, character.only = TRUE)
-        .Cluster = makeCluster(workers, outfile = "./multithread.log")
-        assign(".Cluster", .Cluster, envir = .GlobalEnv)
-        registerDoSNOW(.Cluster)
-
-        message(sprintf("Registered %d nodes for parallel execution with doSNOW", length(.Cluster)))
-
-        return(TRUE)
-    }
-    else if (workers <= 1)
-        message(sprintf("Not enough workers. (Supplied value is '%g')", workers))
-    else {
-
-        s = "Parallel backend packages are not installed.\r\nMissing packages:"
-        index = 1
-        for (pckg in setdiff(Parallel.Packages(), installed.packages())) {
-            s = sprintf("%s\r\n%2.0f. %s", s, index, pckg)
-            index = index + 1
-        }
-
-        s = sprintf("%s\r\n", s)
-
-        message(s)
-    }
-
-    return(FALSE)
-
-}
-
-Parallel.TryDispose = function() {
-    if (!exists(".Cluster")) {
-        message("Cluster is not initialized.")
-        return(FALSE)
-    }
-    else if (!"cluster" %in% class(.Cluster)) {
-        message("Backend '.Cluster' variable is of different type. Aborting.")
-        return (FALSE)
-    }
-        
-
-    if (!("parallel" %in% sessionInfo()[[5]]) || !exists("stopCluster")) {
-        if ("parallel" %in% installed.packages()) {
-            message("Loading package 'parallel'")
-            library("parallel")
-        }
-        
-        else {
-            message("Package 'parallel' is unavailable. Aborting.")
-            return (FALSE)
-        }
-    }
-
-    stopCluster(.Cluster)
-
-    message("Cluster disposed.")
-
-    rm(".Cluster", envir = .GlobalEnv)
-
-    return (TRUE)
-}
-
-Parallel.BackendExists = function()
-    return(exists(".Cluster") && ("cluster" %in% class(.Cluster)))
-
-Parallel.Lapply = function(x, fun, ..., export = c()) {
+#Parallel.Lapply = function(x, fun, ..., export = c()) {
    
-    if (!Parallel.BackendExists())
-        stop("No parallel backend detected.")
+    #if (!Parallel.BackendExists())
+        #stop("No parallel backend detected.")
 
-    else {
-        globals = ls(envir = .GlobalEnv)
-        export = c(export, globals[grepl("Parallel", globals)])
+    #else {
+        #globals = ls(envir = .GlobalEnv)
+        #export = c(export, globals[grepl("Parallel", globals)])
         
-        clusterExport(.Cluster, export)
-        return(foreach(
-            locX = x,
-        #.verbose = TRUE,
-        .final = function(lst) { names(lst) = x; return(lst) },
-        .multicombine = TRUE,
-        .export = export)
-        %dopar% fun(locX, ...))
+        #clusterExport(.Cluster, export)
+        #return(foreach(
+            #locX = x,
+        ##.verbose = TRUE,
+        #.final = function(lst) { names(lst) = x; return(lst) },
+        #.multicombine = TRUE,
+        #.export = export)
+        #%dopar% fun(locX, ...))
 
 
-    }
+    #}
 
-}
+#}
+.symbs = c(97:122, 65:90, 48:57)
+setOldClass("cluster")
+setOldClass("SOCKcluster")
+Cluster = setRefClass("Cluster",
+    fields = list(
+        ClusterDesc = "SOCKcluster",        
+        IsRegistered = "logical",
+        IsDisposed = "logical",
+        ID = "character"))
+Cluster$lock("ClusterDesc", "ID")
+
+Cluster$methods("initialize" = function(nProcs) {
+    if (!is.numeric(nProcs) || nProcs < 0 || nProcs > detectCores())
+        stop("Illegal number of processes requested.")
+    ClusterDesc <<- makeCluster(nProcs, "SOCK")
+    IsRegistered <<- FALSE
+    IsDisposed <<- FALSE
+    ID <<- paste("@", rawToChar(as.raw(sample(.symbs, 8))), sep = "")
+    message(sprintf("Cluster %s (%d nodes) was created.", .self$ID, .self$getClusterSize()))
+})
+
+Cluster$methods("getClusterDesc" = function() .self$ClusterDesc)
+Cluster$methods("getIsRegistered" = function() .self$IsRegistered)
+Cluster$methods("getClusterSize" = function() length(.self$ClusterDesc))
+Cluster$methods("getID" = function() length(.self$ID))
+
+Cluster$methods("Register" = function(backend = "SNOW")
+{
+    initCall = paste("registerDo", backend, sep = "")
+    if (exists(initCall))
+        eval(as.name(initCall))(ClusterDesc)
+
+    IsRegistered <<- TRUE
+    message(sprintf("Cluster %s (%d nodes) was registered.", .self$ID, .self$getClusterSize()))
+})
+
+Cluster$methods("Dispose" = function()
+{
+    stopCluster(ClusterDesc)
+    IsDisposed <<- TRUE
+    message(sprintf("Cluster %s (%d nodes) was disposed.", .self$ID, .self$getClusterSize()))
+})
+
+Cluster$methods("finalize" = function()
+    { if (!.self$IsDisposed)
+        .self$Dispose()
+})
