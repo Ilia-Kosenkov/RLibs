e349ac513ab2371eb4d81abf3a5b6a724c155667
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2018-04-26 22:58:26 +0300

    Helper funciont tested

--- Compile.R
+++ Compile.R
@@ -1,6 +1,20 @@
 if (interactive()) {
     # If is in interactive session, run debug code
 
+    test_BSTools.DebugPlot <- function() {
+        require(dplyr)
+        N <- 10000
+        data <- list(
+            tibble(x = runif(N), y = rnorm(N), z = rnorm(N, 1, 100), test = rbeta(N, 1, 2)),
+            tibble(x = runif(N), y = rnorm(N), z = rnorm(N, 1, 100), test = rbeta(N, 1, 2)),
+            tibble(x = runif(N), y = rnorm(N), z = rnorm(N, 1, 100), test = rbeta(N, 1, 2)),
+            tibble(x = runif(N), y = rnorm(N), z = rnorm(N, 1, 100), test = rbeta(N, 1, 2)))
+
+        BSTools.DebugPlot(data)
+    }
+
+    test_BSTools.DebugPlot()
+
 } else {
     # If session is not interactive, build package
     require(devtools)
