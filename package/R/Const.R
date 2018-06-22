a0b3938c0fb8a9364a5bff0a683b7348cf2f5b11
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-22 08:55:07 +0200

    Styling

--- package/R/Const.R
+++ package/R/Const.R
@@ -22,45 +22,45 @@
 #   THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #' @export
-Const.c = 2.99792458e10
+Const.c <- 2.99792458e10
 #' @export
-Const.h = 6.6260755e-27
+Const.h <- 6.6260755e-27
 #' @export
-Const.h_bar = 1.05457266e-27
+Const.h_bar <- 1.05457266e-27
 #' @export
-Const.G = 6.67259e-8
+Const.G <- 6.67259e-8
 #' @export
-Const.K = 1.380658e-16
+Const.K <- 1.380658e-16
 #' @export
-Const.m_e = 9.10938356e-28
+Const.m_e <- 9.10938356e-28
 #' @export
-Const.m_H = 1.6737236e-24
+Const.m_H <- 1.6737236e-24
 
 #' @export
-Const.m_Sun = 1.98855e33
+Const.m_Sun <- 1.98855e33
 #' @export
-Const.L_Sun = 3.828e33
+Const.L_Sun <- 3.828e33
 #' @export
-Const.R_Sun = 6.95700e10
+Const.R_Sun <- 6.95700e10
 
 #' @export
-Const.Sigm_SB = 5.67036713e-5
+Const.Sigm_SB <- 5.67036713e-5
 #' @export
-Const.Sigm_Th = 6.6524587158e-25
+Const.Sigm_Th <- 6.6524587158e-25
 #' @export
-Const.g = 9.80665e2
+Const.g <- 9.80665e2
 #' @export
-Const.R_gas = 8.314459848e7
+Const.R_gas <- 8.314459848e7
 
 
 # Unit conversions
 #' @export
-Const.Jy2CGC = 1e-23
+Const.Jy2CGC <- 1e-23
 
 # Alias
 #' @export
-Const.SB_sigm = Const.Sigm_SB
+Const.SB_sigm <- Const.Sigm_SB
 #' @export
-Const.Th_sigm = Const.Sigm_Th
+Const.Th_sigm <- Const.Sigm_Th
 #' @export
-Const.M_Sun = Const.m_Sun
+Const.M_Sun <- Const.m_Sun
