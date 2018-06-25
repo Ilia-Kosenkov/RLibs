a1cfe8512055780261248d7e757a10e46b5362af
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-25 10:14:38 +0200

    Compilation proces improved

--- Compile.R
+++ Compile.R
@@ -18,9 +18,27 @@ if (interactive()) {
         dplyr::slice(1) %>%
         dplyr::pull(File) -> latestPckg
 
-    command <- sprintf("cmd /k \"R.exe CMD build ./package && R.exe CMD check %s && exit\"",
-        latestPckg)
-    message(paste("Executing:", command))
-    system(command)
+
+    isWin <- grepl("win(dows)?", Sys.info()["sysname"])
+    if (is.na(isWin))
+        stop("Unable to detect system. Run `R CMD build` manually.")
+
+    sfx <- ifelse(isWin, ".exe", "")
+
+    cmd_1 <- sprintf("R%s CMD build ./package", sfx)
+    cmd_2 <- sprintf("R%s CMD check %s", sfx, latestPckg)
+    #command <- sprintf("cmd /k \"R.exe CMD build ./package && R.exe CMD check %s && exit\"",
+        #latestPckg)
+    message(paste("Executing:", cmd_1))
+    if(isWin)
+        shell(cmd_1, mustWork = TRUE)
+    else
+        system(cmd_1)
+    message(paste("Executing:", cmd_2))
+    if(isWin)
+        shell(cmd_2, mustWork = TRUE)
+    else
+        system(cmd_2)
+
 
 }
