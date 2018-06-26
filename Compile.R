c53e380a0927dca7b47a465ebdf6f7d34b17793c
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-26 10:55:46 +0200

    Changed the order of compilation

--- Compile.R
+++ Compile.R
@@ -29,6 +29,18 @@ if (interactive()) {
     message("Running `roxygen2::roxygenize`...")
     roxygen2::roxygenize("./package")
 
+    isWin <- grepl("win(dows)?", Sys.info()["sysname"])
+    if (is.na(isWin))
+        stop("Unable to detect system. Run `R CMD build` manually.")
+    sfx <- ifelse(isWin, ".exe", "")
+    cmd_1 <- sprintf("R%s CMD build ./package", sfx)
+
+    message(paste("Executing:", cmd_1))
+    if(isWin)
+        shell(cmd_1, mustWork = TRUE)
+    else
+        system(cmd_1)
+
     pckgs <- base::dir(".", "*.gz")
 
     `%>%` <- dplyr::`%>%`
@@ -42,21 +54,11 @@ if (interactive()) {
         dplyr::pull(File) -> latestPckg
 
 
-    isWin <- grepl("win(dows)?", Sys.info()["sysname"])
-    if (is.na(isWin))
-        stop("Unable to detect system. Run `R CMD build` manually.")
 
-    sfx <- ifelse(isWin, ".exe", "")
 
-    cmd_1 <- sprintf("R%s CMD build ./package", sfx)
     cmd_2 <- sprintf("R%s CMD check %s", sfx, latestPckg)
     #command <- sprintf("cmd /k \"R.exe CMD build ./package && R.exe CMD check %s && exit\"",
         #latestPckg)
-    message(paste("Executing:", cmd_1))
-    if(isWin)
-        shell(cmd_1, mustWork = TRUE)
-    else
-        system(cmd_1)
     message(paste("Executing:", cmd_2))
     if(isWin)
         shell(cmd_2, mustWork = TRUE)
