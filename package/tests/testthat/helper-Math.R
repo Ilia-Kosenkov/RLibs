b47b703f01eaa99cf0e4906d749bbda560542c67
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-01-20 02:37:23 +0200

    Import for math tests

--- package/tests/testthat/helper-Math.R
+++ package/tests/testthat/helper-Math.R
@@ -21,6 +21,7 @@
 #   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
 #   THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+library(rlang)
 if (interactive()) {
     source(file.path("..", "..", "R", "Math.R"))
     source(file.path("..", "..", "R", "are_equal_f.R"))
