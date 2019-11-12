000b063ae55780fc4422368d853d3aed31e06d18
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-11-12 15:50:06 +0200

    Conditional join

--- R/joins.R
+++ R/joins.R
@@ -121,3 +121,87 @@ safe_join_coalesce <- function(x, y, by) {
 
     return(list(x = mutate(x, !!!x_mod), y = mutate(y, !!!y_mod), by = bys))
 }
+
+
+#' @title join_condition
+#'
+#' @param left LHS table.
+#' @param right RHS table
+#' @param ... Joining conditions, where \code{.x} refers to RHS table and
+#' \code{.y} referes to LHS table. E.g. \code{.x$hp > .y$mpg}.
+#' Comma-separated conditions are \code{`&`}.
+#' @param .type If multiple matches, which RHS to select
+#' @param .suffix
+#' @param .enforce_suffix
+#'
+#' @return
+#' @export
+#'
+#' @examples
+join_condition <- function(left, right, ...,
+    .type = "first", .suffix = c("__l", "__r"),
+    .enforce_suffix = FALSE) {
+
+    cond <- enquos(...)
+
+    if (vec_is(.type, character(), 1L)) {
+        .type <- tolower(.type)
+        if (.type == "first")
+            selector <- function(x) head(x, 1)
+        else if (.type == "last")
+            selector <- function(x) tail(x, 1)
+        else
+            abort("Error", "maxi2_invalid_argument")
+        }
+    else
+        selector <- as_function(.type)
+
+    cond <- map(cond, function(cnd) {
+        if (!quo_is_call(cnd))
+            abort("Error", "maxi2_invalid_argument")
+
+        expr <- quo_get_expr(cnd)
+        expr <- expr(outer(!!expr[[2]], !!expr[[3]], !!expr[[1]]))
+        quo_set_expr(cnd, expr)
+    })
+
+
+    cond %>%
+        map(eval_tidy, list(.x = left, .y = right)) %>%
+        map(which, arr.ind = TRUE) %>%
+        map(as_tibble, .name_repair = "minimal") %>%
+        reduce(inner_join) -> indices
+
+
+    left <- slice(left, indices$row)
+    right <- slice(right, indices$col)
+
+    if (.enforce_suffix) {
+        left <- set_names(left, paste0(names(left), .suffix[1]))
+        right <- set_names(right, paste0(names(right), .suffix[2]))
+    }
+    else {
+        common_names <- which(vec_in(names(right), names(left)))
+        if (!vec_is_empty(common_names)) {
+            left <- set_names(left,
+                flatten_chr(map_at(
+                    names(left),
+                    which(vec_in(names(left), names(right)[common_names])),
+                    paste0, .suffix[1])))
+            right <- set_names(right,
+                flatten_chr(map_at(
+                    names(right),
+                    common_names,
+                    paste0, .suffix[2])))
+        }
+
+    }
+
+    bind_cols(left, right)
+}
+
+tbl <- tibble(x = cc(10, 12, 15, 17, 20, 25, 30), y = x + 5, c = letters[1:7])
+
+join_condition(mtcars[1:10,], tbl, .x$mpg >= .y$x, .x$mpg < .y$y,
+    .type = ~.x[1]) %>%
+    print
