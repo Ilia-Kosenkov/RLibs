d69dc24d9a5b6e325d1169d52b9cc7d65047a0aa
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-12-14 00:44:34 +0200

    Corrected compilation file using gist

--- Compile.R
+++ Compile.R
@@ -23,61 +23,61 @@
 
 
 if (interactive()) {
-        # Library imports for debug go here
-        #
-        library(tidyverse)
-        library(scales)
-        library(grid)
-        library(gridExtra)
-        library(magrittr)
-        library(rlang)
-        library(glue)
-        library(foreach)
-        library(feather)
-        library(fs)
-        library(assertthat)
-        library(vctrs)
-        library(zeallot)
+    # Library imports for debug go here
+    #
+    library(tidyverse)
+    library(scales)
+    library(grid)
+    library(gridExtra)
+    library(magrittr)
+    library(rlang)
+    library(glue)
+    library(foreach)
+    library(feather)
+    library(fs)
+    library(assertthat)
+    library(vctrs)
+    library(zeallot)
 
-        purrr::walk(fs::dir_ls("R", glob = "*R"), source)
-    } else {
+    purrr::walk(fs::dir_ls("R", glob = "*R"), source)
+} else {
 
-        message("Running `roxygen2::roxygenize`...")
-        roxygen2::roxygenize(".")
-        message("Finished `roxygen2::roxygenize`...")
+    message("Running `roxygen2::roxygenize`...")
+    roxygen2::roxygenize(".")
+    message("Finished `roxygen2::roxygenize`...")
 
-        is_win <- grepl("win(dows)?", Sys.info()["sysname"])
-        if (is.na(is_win))
-            stop("Unable to detect system. Run `R CMD build` manually.")
-        sfx <- ifelse(is_win, ".exe", "")
-        cmd_1 <- sprintf("R%s CMD build .", sfx)
+    is_win <- grepl("[Ww]in(dows)?", Sys.info()["sysname"])
+    if (is.na(is_win))
+        stop("Unable to detect system. Run `R CMD build` manually.")
+    sfx <- ifelse(is_win, ".exe", "")
+    cmd_1 <- sprintf("R%s CMD build .", sfx)
 
-        message(paste("Executing:", cmd_1))
-        if (is_win)
-            shell(cmd_1, must_wrok = TRUE)
-        else
-            system(cmd_1)
+    message(paste("Executing:", cmd_1))
+    if (is_win)
+        shell(cmd_1, mustWork = TRUE)
+    else
+        system(cmd_1)
 
-        pckgs <- fs::dir_ls(".", glob = "*.gz")
+    pckgs <- fs::dir_ls(".", glob = "*.gz")
 
-        `%>%` <- dplyr::`%>%`
+    `%>%` <- dplyr::`%>%`
 
-        stringr::str_match(pckgs, "^.*_((?:[0-9]+?\\.?){3})\\.tar\\.gz$") %>%
-        dplyr::as_tibble(.name_repair = ~c("File", "Version")) %>%
-        dplyr::mutate(
-            VersionNum = stringr::str_split(Version, "\\."),
-            Major = purrr::map_int(VersionNum, ~ readr::parse_integer(.x[1])),
-            Minor = purrr::map_int(VersionNum, ~ readr::parse_integer(.x[2])),
-            Patch = purrr::map_int(VersionNum, ~ readr::parse_integer(.x[3]))) %>%
-        dplyr::arrange(desc(Major), desc(Minor), desc(Patch)) %>%
-        dplyr::slice(1) %>%
-        dplyr::pull(File) -> latest_pckg
+    stringr::str_match(pckgs, "^.*_((?:[0-9]+?\\.?){3})\\.tar\\.gz$") %>%
+    dplyr::as_tibble(.name_repair = ~c("File", "Version")) %>%
+    dplyr::mutate(
+        VersionNum = stringr::str_split(Version, "\\."),
+        Major = purrr::map_int(VersionNum, ~ readr::parse_integer(.x[1])),
+        Minor = purrr::map_int(VersionNum, ~ readr::parse_integer(.x[2])),
+        Patch = purrr::map_int(VersionNum, ~ readr::parse_integer(.x[3]))) %>%
+    dplyr::arrange(desc(Major), desc(Minor), desc(Patch)) %>%
+    dplyr::slice(1) %>%
+    dplyr::pull(File) -> latest_pckg
 
 
-        cmd_2 <- sprintf("R%s CMD check %s", sfx, latest_pckg)
-        message(paste("Executing:", cmd_2))
-        if (is_win)
-            shell(cmd_2, must_wrok = TRUE)
-        else
-            system(cmd_2)
-        }
+    cmd_2 <- sprintf("R%s CMD check %s", sfx, latest_pckg)
+    message(paste("Executing:", cmd_2))
+    if (is_win)
+        shell(cmd_2, mustWork = TRUE)
+    else
+        system(cmd_2)
+}
